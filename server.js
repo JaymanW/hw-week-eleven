a573c91eaa2383cf607e2aa9379e8a870e276345
JaymanW <jaymanw2000@gmail.com>
2021-04-27 13:11:05 -0500

    HTML page routes

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+
+const path = require('path');
+
 const PORT = 8080;
 
 app.use(cors());
 app.use(express.static('public/'));
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
+// ROUTES
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/notes.html'));
+    // res.sendFile(path.join(__dirname, 'public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on port: ${PORT}`);
