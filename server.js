3a494187bd470fe64fb8f8bebbcf95b257fe4129
JaymanW <jaymanw2000@gmail.com>
2021-04-27 13:19:04 -0500

    notes read from db

--- server.js
+++ server.js
@@ -1,26 +1,32 @@
 const express = require('express');
 const cors = require('cors');
-const app = express();
-
 const path = require('path');
+const fs = require('fs');
+
+const app = express();
 
 const PORT = 8080;
 
 app.use(cors());
 app.use(express.static('public/'));
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
 
 // ROUTES
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/notes.html'));
-    // res.sendFile(path.join(__dirname, 'public/notes.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+    fs.readFile(path.join(__dirname, 'db/db.json'), 'utf8', (err, data) => {
+        res.json(JSON.parse(data));
+    });
 });
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
+
+// SERVER LISTEN
 app.listen(PORT, () => {
     console.log(`Server running on port: ${PORT}`);
 });
