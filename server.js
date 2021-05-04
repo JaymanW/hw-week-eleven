b353bdfa181e70400a8f668ff8841155f1b16cfe
JaymanW <jaymanw2000@gmail.com>
2021-05-04 18:35:21 -0500

    default server route

--- server.js
+++ server.js
@@ -80,6 +80,10 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
 
 // SERVER LISTEN
 app.listen(PORT, () => {
